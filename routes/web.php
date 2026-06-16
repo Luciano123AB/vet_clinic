9b8945840d5bae801506382fd03c34bd2ccac7d2
Luciano Eduardo <luciano123ab@gmail.com>
2026-06-16 12:58:14 -0300

    005(Index_ClientController)

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\ClientController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/client', [ClientController::class, 'index']);
