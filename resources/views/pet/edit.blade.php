ec4164a24c63d9124766e3791b2836587065be5d
Luciano Eduardo <luciano123ab@gmail.com>
2026-06-23 13:16:31 -0300

    034(EditPet_Parte2)

--- resources/views/pet/edit.blade.php
+++ resources/views/pet/edit.blade.php
@@ -2,7 +2,7 @@
 
 @section('body')
     <div class="alert alert-primary" role="alert">
-        <h2>New Pet</h2>
+        <h2>Edit Pet</h2>
     </div>
 
     <section class="content">
@@ -12,13 +12,17 @@
                     <br>
 
                     <div class="box-body no-padding">
-                        <form action="/pet" method="post" role="form" enctype="multipart/form-data">
+                        <form action="/pet/edit/{{ $thePet->id }}" method="post" role="form" enctype="multipart/form-data">
                             @csrf
 
                             <div class="box-body">
                                 <div class="form-group">
                                     <label for="name">Nome</label>
-                                    <input type="text" class="form-control" id="name" name="name" oninvalid="this.setCustomValidity('Campo requerido.')" onchange="try{setCustomValidity('')}catch(e){}" required>
+                                    <input type="text" class="form-control" id="name" name="name" oninvalid="this.setCustomValidity('Campo requerido.')" onchange="try{setCustomValidity('')}catch(e){}" value="{{ $thePet->name }}" required>
+                                </div>
+                                <div class="form-check">
+                                    <input type="checkbox" class="form-check-input" id="no_photo" name="no_photo">
+                                    <label for="no_photo" class="no_photo">Sem Foto</label>
                                 </div>
                                 <div class="form-group">
                                     <label for="photo">Foto</label>
@@ -26,19 +30,19 @@
                                 </div>
                                 <div class="form-group">
                                     <label for="specie">Espécie</label>
-                                    <input type="text" class="form-control" id="specie" name="specie">
+                                    <input type="text" class="form-control" id="specie" name="specie" value="{{ $thePet->specie }}">
                                 </div>
                                 <div class="form-group">
                                     <label for="breed">Raça</label>
-                                    <input type="text" class="form-control" id="breed" name="breed">
+                                    <input type="text" class="form-control" id="breed" name="breed" value="{{ $thePet->breed }}">
                                 </div>
                                 <div class="form-group">
                                     <label for="color">Cor</label>
-                                    <input type="text" class="form-control" id="color" name="color">
+                                    <input type="text" class="form-control" id="color" name="color" value="{{ $thePet->color }}">
                                 </div>
                                 <div class="form-group">
                                     <label for="height">Altura</label>
-                                    <input type="number" class="form-control" id="height" name="height" step="0.001" value="0.000" placeholder="0.000">
+                                    <input type="number" class="form-control" id="height" name="height" step="0.001" value="0.000" placeholder="0.000" value="{{ $thePet->height }}">
                                 </div>
                                 <div class="form-group">
                                     <label for="weight">Peso</label>
@@ -57,15 +61,15 @@
                                 </div>
                                 <div class="form-group">
                                     <label for="father">Pai</label>
-                                    <input type="text" class="form-control" id="father" name="father">
+                                    <input type="text" class="form-control" id="father" name="father" value="{{ $thePet->father }}">
                                 </div>
                                 <div class="form-group">
                                     <label for="mother">Mãe</label>
-                                    <input type="text" class="form-control" id="mother" name="mother">
+                                    <input type="text" class="form-control" id="mother" name="mother" value="{{ $thePet->mother }}">
                                 </div>
                                 <div class="form-group">
                                     <label>Observações</label>
-                                    <textarea class="form-control" id="observations" name="observations" rows="4"></textarea>
+                                    <textarea class="form-control" id="observations" name="observations" rows="4" value="{{ $thePet->observations }}"></textarea>
                                 </div>
                             </div>
                             <br>
