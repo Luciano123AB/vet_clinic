875bb6cc991af4349db6270e2605a84cfe170111
Luciano Eduardo <luciano123ab@gmail.com>
2026-06-23 09:29:59 -0300

    024(CriacaoModel_Pet)

---
+++ database/migrations/2026_06_23_121806_create_pets_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pets', function (Blueprint $table) {
+            $table->id();
+            $table->string('photo_path')->nullable(); //Foto
+            $table->string('name'); //Nome
+            $table->string('specie')->nullable(); //Espécie
+            $table->string('breed')->nullable(); //Raça
+            $table->string('color')->nullable(); //Cor
+            $table->decimal('height', 12, 3)->nullable(); //Altura
+            $table->decimal('weight', 12, 3)->nullable(); //Peso
+            $table->string('gender')->nullable(); //Sexo
+            $table->date('birth_date')->nullable(); //Data de Nascimento
+            $table->string('father')->nullable(); //Pai
+            $table->string('mother')->nullable(); //Mãe
+            $table->text('observations')->nullable(); //Observações
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pets');
+    }
+};
