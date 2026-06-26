f544d9146823d712fcb973d1b3e1db74ff98a010
Luciano Eduardo <luciano123ab@gmail.com>
2026-06-26 14:59:40 -0300

    039(Modelagem_ClientPet)

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Client extends Model
 {
-    //
+    public function pets(): HasMany
+    {
+        return $this->hasMany(Pet::class);
+    }
 }
--- database/migrations/2026_06_23_121806_create_pets_table.php
+++ database/migrations/2026_06_23_121806_create_pets_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('pets', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('client_id')->nullable()->references('id')->on('clients')->onDelete('set null');
             $table->string('photo_path')->nullable(); //Foto
             $table->string('name'); //Nome
             $table->string('specie')->nullable(); //Espécie
