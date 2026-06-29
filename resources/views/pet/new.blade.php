1b3d1ac2191babc30850c29d02448c09ad97d4a7
Luciano Eduardo <luciano123ab@gmail.com>
2026-06-29 11:59:05 -0300

    044(AjustandoNewPet_Parte5)

--- resources/views/pet/new.blade.php
+++ resources/views/pet/new.blade.php
@@ -73,10 +73,10 @@
                                     <input type="text" class="form-control" id="name" name="name" oninvalid="this.setCustomValidity('Campo requerido.')" onchange="try{setCustomValidity('')}catch(e){}" required>
                                 </div>
                                 <div class="form-group">
-                                    <input type="text" class="form-control" id="id_client" name="id_client" required>
+                                    <input type="hidden" class="form-control" id="id_client" name="id_client" required>
                                 </div>
                                 <div class="form-group">
-                                    <label for="client_pet">Client:</label>
+                                    <label for="client_pet">Cliente:</label>
                                     <input type="text" class="form-control" id="client_pet" name="client_pet" onkeydown="return false" required>
                                 </div>
                                 <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalClientPet">
