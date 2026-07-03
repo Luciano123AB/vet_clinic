071ee6611bf6e7e50665facb1d653f172a06de8d
Luciano Eduardo <luciano123ab@gmail.com>
2026-07-03 13:31:50 -0300

    062(Tabela_ConsultationsProcedures)

--- app/Models/Vet.php
+++ app/Models/Vet.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Vet extends Model
 {
-    //
+    public function consultation(): HasMany
+    {
+        return $this->hasMany(Consultation::class);
+    }
 }
