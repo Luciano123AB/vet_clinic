dc2f97beff2e5b7bde0c4d40bb7f59b786ca56ce
Luciano Eduardo <luciano123ab@gmail.com>
2026-07-10 15:30:47 -0300

    070(NewConsultation_Parte5)

--- resources/views/consultation/new.blade.php
+++ resources/views/consultation/new.blade.php
@@ -54,6 +54,51 @@
         </div>
     </div>
     {{-- FIM - Modal Veterinário --}}
+    {{-- Modal Pet --}}
+    <div class="modal fade" id="modalPets" tabindex="-1" aria-labelledby="modalPetsLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="modalVetsLabel">Pets</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <table id="grid_pet" class="table table-bordered table-striped">
+                        <thead>
+                            <tr>
+                                <th style="width: 10px;">#</th>
+                                <th>Name</th>
+                                <th>Owner</th>
+                                <th>Specie</th>
+                                <th>Gender</th>
+                                <th style="width: 140px;">&nbsp;</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($pets as $pet)
+                                <tr>
+                                    <td>{{ $pet->id }}</td>
+                                    <td>{{ $pet->name }}</td>
+                                    <td>{{ $pet->client->name }}</td>
+                                    <td>{{ $pet->specie }}</td>
+                                    <td>{{ $pet->gender }}</td>
+                                    <td>
+                                        <button type="button" class="btn btn-xs btn-primary" data-bs-toggle="tooltip" data-placement="top" onclick="add_pet(this)">
+                                            <i class="fa fa-plus"></i> Selecionar
+                                        </button>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    {{-- FIM - Modal Pet --}}
 
     <div class="alert alert-primary" role="alert">
         <h2>New Consultation</h2>
@@ -81,12 +126,17 @@
                                 </button>
 
                                 <div class="form-group">
-                                    <input type="text" class="form-control" id="id_vet" name="id_vet" readonly>
+                                    <input type="hidden" class="form-control" id="id_vet" name="id_vet" readonly>
                                 </div>
                                 <div class="form-group">
                                     <label for="vet_name">Name:</label>
                                     <input type="text" class="form-control" id="vet_name" name="vet_name" required readonly>
                                 </div>
+
+                                <h3 class="text-danger">Pet:</h3>
+                                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalPets">
+                                    Selecione o Pet
+                                </button>
 
                                 <div class="form-group">
                                     <label for="email">Email</label>
