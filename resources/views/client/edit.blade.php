be6d11f6146c8fb25a639345dfdbb49d9566577a
Luciano Eduardo <luciano123ab@gmail.com>
2026-07-24 14:19:38 -0300

    096(Ajustando_PaginaInicial)

--- resources/views/client/edit.blade.php
+++ resources/views/client/edit.blade.php
@@ -2,7 +2,7 @@
 
 @section('body')
     <div class="alert alert-warning" role="alert">
-        <h2>Edit Client</h2>
+        <h2>Editar Cliente</h2>
     </div>
 
     <section class="content">
@@ -33,7 +33,7 @@
                                     <input type="text" class="form-control" id="address" name="address" value="{{ $theClient->address }}">
                                 </div>
                                 <div class="form-group">
-                                    <label>Estado (states of Brazil):</label>
+                                    <label>Estado (estados do Brazil):</label>
                                     <select class="form-control" id="state" name="state">
                                         <option value="AC" @if ($theClient->state == 'AC') {{ 'selected' }} @endif>AC</option>
                                         <option value="AL" @if ($theClient->state == 'AL') {{ 'selected' }} @endif>AL</option>
@@ -68,7 +68,7 @@
                             <br>
 
                             <div class="box-footer">
-                                <button type="submit" class="btn btn-success">Save</button>
+                                <button type="submit" class="btn btn-success">Salvar</button>
                             </div>
                         </form>
                     </div>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,15 @@
 @extends('layout.main')
 
 @section('body')
-    <h1>Página Inicial</h1>    
+    <div class="alert alert-success text-center" role="alert">
+        <h2>Clínica Veterinária</h2>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <img src="{{ url('/main-image.jpg') }}" style="max-width: 95%;" class="mx-auto d-block" alt="imagem principal">
+            </div>
+        </div>
+    </div>
 @endsection
